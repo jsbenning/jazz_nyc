718a3f96dd8c27abdc457084f25342b1a80c2640
Joe <jsbenning4@gmail.com>
2016-06-23 16:44:49 -0400

    updated environment files

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,7 @@ require 'colorize'
 
 require_relative "../lib/jazz_nyc/version"
 require_relative "../lib/jazz_nyc/cli"
-require_relative "../lib/jazz_nyc/scraper"
 require_relative "../lib/jazz_nyc/menu"
+require_relative "../lib/jazz_nyc/event"
+require_relative "../lib/jazz_nyc/smalls_scraper"
+require_relative "../lib/jazz_nyc/van_scraper"
